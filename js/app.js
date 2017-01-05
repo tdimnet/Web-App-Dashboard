cbaa8f9794b4880a11042c1b4333a70a93480647
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-01-05 06:59:12 +0100

    Create a dropdown menu for the notification bell element and add some style on it. Also add the library chartjs and start using it.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+//The dropdown notification menu
+
+
+//Close the alert dialog box
 $('.close-button').click(function() {
   $('.alert-box').fadeOut("slow");
 });
+
+//The web traffic widget
+  //Selecting the canvas "myChart" element
